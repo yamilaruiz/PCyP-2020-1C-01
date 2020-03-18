82c0efe69fdf3867f4ac7ddd0b94d3811e0fde94
Yamila Elizabeth Ruiz <31075784+yamilaruiz@users.noreply.github.com>
2020-03-18 18:03:27 -0300

    Added the second exercise (.csv file)

---
+++ Archivo CSV/ReadFile/ReadFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\vial.csv"; 
+            using (StreamReader file = new StreamReader(path))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.ReadLine();
+            }
+        }
+    }
+}
