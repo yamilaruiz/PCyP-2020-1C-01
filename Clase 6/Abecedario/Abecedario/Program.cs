bc090ff6d6d5f9e88d6aaba7a0b3f2ca0f1f3303
Yamila Elizabeth Ruiz <31075784+yamilaruiz@users.noreply.github.com>
2020-04-08 12:19:50 -0300

    Added exercise Abecedario with lambdas

---
+++ Clase 6/Abecedario/Abecedario/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Abecedario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letra = 'a';
+
+            var p1 = Task.Factory.StartNew( () => LeerAbecedario2(letra));
+            var p2 = Task.Factory.StartNew( () => LeerAbecedario1(letra));
+
+            Console.ReadLine();
+        }
+        
+        public static void LeerAbecedario1(char letra)
+        {
+            string tarea2 = null;
+            while (letra <= 'z')
+            {
+                if ((letra % 2) == 0)
+                {
+                    tarea2 = tarea2 + " "+ letra;
+
+                }
+                letra++;
+            }
+            Console.WriteLine(tarea2);
+
+        }
+
+        public static void LeerAbecedario2(char letra)
+        {
+            string tarea2 = null;
+            while (letra <= 'z')
+            {
+                if ((letra % 2) != 0)
+                {
+                    tarea2 = tarea2 + " " + letra;
+
+                }
+                letra++;
+            }
+            Console.WriteLine(tarea2);
+
+        }
+
+    }
+}
